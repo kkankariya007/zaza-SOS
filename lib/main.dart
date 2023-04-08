dd64673636931bed877214717d47b002a98e4ac1
Kunal Kankariya <kkankariya007@gmail.com>
2023-04-08 17:28:47 +0530

    Updates Pushed

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,55 @@ class _SOSState extends State<SOS> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.transparent,
       title: Text('Emergency'),
-),
-      body: Column(
-      children: [
-        Text('ds'),
-      ],
+        elevation: 0,
       ),
+
+      body:
+      Center(
+      child:Container(decoration: const BoxDecoration(
+      gradient: LinearGradient(
+      begin: Alignment.topRight,
+      end: Alignment.bottomLeft,
+      colors: [
+        Colors.purpleAccent,
+        Colors.redAccent,
+        ],
+      ),
+      ),
+
+      child:Center(
+      child: Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+      SizedBox(height: 200.0),
+
+        RawMaterialButton(
+        constraints: BoxConstraints.tight(Size(260,260)),
+        fillColor: Colors.black.withOpacity(0.68),
+      child: Center(
+        child: Text('Send Noodes',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold, 
+        ),),
+      ),
+      onPressed: () async {},
+        elevation: 10.0,
+      padding: const EdgeInsets.all(30.0),
+      shape: const CircleBorder(),
+    ),
+
+
+
+                      ],
+                  ),
+                  ),
+            ),
+      ),
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.transparent,
     );
   }
 }
